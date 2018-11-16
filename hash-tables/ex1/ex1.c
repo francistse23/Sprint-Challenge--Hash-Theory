727b7857e0f5633bec3efb6f2dd703f3c2ec1b3f
francistse23 <wtse.ucsd@gmail.com>
2018-11-16 08:57:16 -0800

    hashtable ex1 completed. test passed.

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -6,8 +6,25 @@
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
+  Answer *answer = malloc(sizeof(Answer));
 
-  // YOUR CODE HERE
+  for (int i = 0; i < length; i++){
+    hash_table_insert(ht, weights[i], i);
+  }
+
+  for (int j = 0; j < length; j++){
+    int index = hash_table_retrieve(ht, limit-weights[j]);
+    if (index != -1){
+      if (j > index){
+        answer->index_1 = j;
+        answer->index_2 = index;
+      }
+      answer->index_1 = index;
+      answer->index_2 = j;
+      destroy_hash_table(ht);
+      return answer;
+    }
+  }
 
   return NULL;
 }
